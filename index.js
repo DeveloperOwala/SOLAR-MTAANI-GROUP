9f930df6dbb5dd896ca63723e08532fa25744bd2
Melvin Omega <melvinomega151@gmail.com>
2021-08-05 00:52:37 +0300

    add toggle effect

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("div#reason").click(function(){
+        $('.reason1-img').slideToggle()
+        $('.reason1').slideToggle()
+    })
+
+
+
+    $("div#reason2").click(function(){
+        $('.reason2-img').slideToggle()
+        $('.reason2').slideToggle()
+    })
+   
+
+    $("div#reason3").click(function(){
+        $('.reason3-img').slideToggle()
+        $('.reason3').slideToggle()
+    })
+
+    $("div#reason4").click(function(){
+        $('.reason4-img').slideToggle()
+        $('.reason4').slideToggle()
+    })
+  
+})
